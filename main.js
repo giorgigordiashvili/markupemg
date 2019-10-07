2023d7e44e666f1afcb17b1bd04109151eb22075
gordela <gordogordel@gmail.com>
2019-10-07 10:55:54 +0400

    file paths

--- main.js
+++ main.js
@@ -35,6 +35,38 @@ $(".responsive").slick({
     // instead of a settings object
   ]
 });
+
+$(".projects-responsive").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1210,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 $(".reviews-slick").slick({
   slidesToShow: 3,
   slidesToScroll: 1,
